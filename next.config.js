68fb595c2ea606f71e4ca7d28e6ec36a76d82403
shabbir3g <mostafizshabbir@gmail.com>
2023-03-29 23:50:56 +0600

    pwa code added for speed optimization

--- next.config.js
+++ next.config.js
@@ -8,13 +8,20 @@ module.exports = withFaust();
 const withPWA = require('next-pwa')({
   dest: 'public'
 })
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
+  pwa: {
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/]
+  },
   optimizeImages: false,
     images: {
-      domains: ['localhost:10004'],
+      domains: ['hy3nzzcq6pe8xlv2r634wluzm.js.wpenginepowered.com'],
     },
     eslint: {
       ignoreDuringBuilds: true,
     }
 });
+
+
