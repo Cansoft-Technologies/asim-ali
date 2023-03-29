94b450d7027b0d4842ac97ab24875799c1300d7d
shabbir3g <mostafizshabbir@gmail.com>
2023-03-30 00:23:24 +0600

    pwa code added and errror solve

--- next.config.js
+++ next.config.js
@@ -8,13 +8,8 @@ module.exports = withFaust();
 const withPWA = require('next-pwa')({
   dest: 'public'
 })
-const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-  pwa: {
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/]
-  },
   optimizeImages: false,
     images: {
       domains: ['hy3nzzcq6pe8xlv2r634wluzm.js.wpenginepowered.com'],
