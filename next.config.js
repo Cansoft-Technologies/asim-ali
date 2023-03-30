f9774bd0a2f97350b5759c73a5672d24d6033714
shabbir3g <mostafizshabbir@gmail.com>
2023-03-30 12:17:55 +0600

    Again change next config file to solve error

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const { withFaust } = require('@faustjs/next');
 /**
  * @type {import('next').NextConfig}
  **/
-// module.exports = withFaust();
 
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+const withPWA = require('next-pwa');
 
 module.exports = withFaust (withPWA({
+  pwa: {
+    dest: "public",
+  },
   optimizeImages: false,
     images: {
       domains: ['hy3nzzcq6pe8xlv2r634wluzm.js.wpenginepowered.com'],
