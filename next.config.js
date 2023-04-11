5a6ca4b30043614cbea4e8060e316496cf156d63
shabbir3g <mostafizshabbir@gmail.com>
2023-04-11 23:38:26 +0600

    added new code cache header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const { withFaust } = require('@faustjs/next');
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
 /**
  * @type {import('next').NextConfig}
@@ -8,7 +10,35 @@ const withPWA = require('next-pwa')({
   dest: 'public'
 })
 
-module.exports = withFaust (withPWA({
+const nextConfig = {
+  images: {
+    domains: ['example.com'],
+    loader: 'imgix',
+    path: 'https://example.com/_next/image',
+  },
+  async headers() {
+    return [
+      {
+        // Define the cache policy for static assets
+        source: '/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+
+
+module.exports = withFaust (withPWA( withPlugins(
+    [[optimizedImages]], 
+    nextConfig
+    ),
+  {
   optimizeImages: false,
     images: {
       domains: ['hy3nzzcq6pe8xlv2r634wluzm.js.wpenginepowered.com'],
@@ -17,5 +47,12 @@ module.exports = withFaust (withPWA({
       ignoreDuringBuilds: true,
     }
 }));
+
+
+
+
+
+
+
 
 
