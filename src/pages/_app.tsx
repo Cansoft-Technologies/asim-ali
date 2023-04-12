a681a29146c770be90be745bdeef96a03cf881e8
shabbir3g <mostafizshabbir@gmail.com>
2023-04-12 23:01:21 +0600

    change color to yellow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import 'scss/main.scss';
 import { client } from 'client';
 import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { cache } from 'next-page-cache';
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
