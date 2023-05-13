466495cb3cb50436d03f3e6886fc479ce528d3a6
shabbir3g <mostafizshabbir@gmail.com>
2023-05-13 21:41:50 +0600

    added gtag code on website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { client } from 'client';
 import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import Script from "next/script";
+import { TRACKING_ID } from "./../../utils/variables";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
@@ -17,6 +19,23 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`}
+      ></Script>
+      {/* 👇 gtag function definition. notice that we don't send page views at this point.  */}
+      <Script
+        id="gtag-init"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', '${TRACKING_ID}');
+          `,
+        }}
+      />
       <FaustProvider client={client} pageProps={pageProps}>
         <Component {...pageProps} />
       </FaustProvider>
