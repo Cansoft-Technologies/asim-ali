715b8a4a1766ac53a0a777610b95304a64171048
PulokSec <riopulok@gmail.com>
2023-06-22 17:38:37 +0600

    fix ui and CWA configurations

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 const { withFaust } = require('@faustjs/next');
 const withOffline = require('next-offline');
-
-
+const runtimeCaching = require("next-pwa/cache");
+const prod = process.env.NODE_ENV === 'production'
 /**
  * @type {import('next').NextConfig}
  **/
 
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  register: true,
+  disable: prod ? false : true,
+  skipWaiting: true,
+  runtimeCaching,
 })
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
