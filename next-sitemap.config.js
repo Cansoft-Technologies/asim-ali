ed21bc106e624f5440ee758784872d2276827e58
Arif <iariful950@gmail.com>
2023-12-13 12:26:49 +0600

    created mortgage broker in campbell page

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -27,6 +27,7 @@ module.exports = {
     "/new-fixed-mortgage-rates-in-bc",
     "/new-mortgage-broker-in-burnaby",
     "/new-mortgage-broker-in-nanaimo",
+    "/new-mortgage-broker-in-campbell-river",
     "/server-sitemap.xml",
     "/location-sitemap.xml",
     ...locationRoutes,
