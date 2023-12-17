62ba0e70e21441c6da9f23bc1757b413e189159f
Arif <iariful950@gmail.com>
2023-12-17 14:32:28 +0600

    used intersection observer for inView loading

--- src/components/WeHelp.tsx
+++ src/components/WeHelp.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Button, Col, Container, Row } from "react-bootstrap";
+import { useInView } from "react-intersection-observer";
 
 type MyProps = {
   helps: any;
@@ -7,27 +8,33 @@ type MyProps = {
 
 const WeHelp = (props: MyProps) => {
   const { helps } = props;
+  const { ref: videoRef, inView: load } = useInView({
+    triggerOnce: true,
+    fallbackInView: true,
+  });
 
   return (
     <>
-      <section className="wehelp_section mb-5">
+      <section ref={videoRef} className="wehelp_section mb-5">
         <Container>
           <Row>
             <Col lg={6}>
-              <div className="wehelp_image">
-                <video
-                  autoPlay
-                  loop
-                  muted
-                  playsInline
-                  style={{ width: "500px", height: "500px" }}
-                >
-                  <source
-                    type="video/mp4"
-                    src={helps?.helpImage?.mediaItemUrl}
-                  />
-                </video>
-              </div>
+              {load && (
+                <div className="wehelp_image">
+                  <video
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    style={{ width: "500px", height: "500px" }}
+                  >
+                    <source
+                      type="video/mp4"
+                      src={helps?.helpImage?.mediaItemUrl}
+                    />
+                  </video>
+                </div>
+              )}
             </Col>
             <Col lg={6}>
               <div>
