2321a1131a7be453aec6dc90e72a21b8be11d80d
Arif <iariful950@gmail.com>
2023-12-18 11:45:37 +0600

    updated code syntax

--- src/components/Meeting.tsx
+++ src/components/Meeting.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { Col, Container, Row } from "react-bootstrap";
 
-
 type MyProps = {
   meetings: any;
 };
@@ -12,58 +11,51 @@ const Meeting = (props: MyProps) => {
   return (
     <>
       <section className="meeting_section">
-
-            <div>
-              {meetings?.hideSection == true ? (
-                ""
-              ) : (
-                <Container>
-                  <Row>
-                    <Col lg={4}>
-                      <div>
-                      <Col>
+        <div>
+          {meetings?.hideSection == true ? (
+            ""
+          ) : (
+            <Container>
+              <Row>
+                <Col lg={4}>
+                  <div>
+                    <Col>
                       <div
                         dangerouslySetInnerHTML={{
-                          __html:
-                            meetings?.meetingTitle,
+                          __html: meetings?.meetingTitle,
                         }}
                       ></div>
                     </Col>
-                      </div>
-                      <div
-                        dangerouslySetInnerHTML={{
-                          __html:
-                            meetings?.meetingDescription,
-                        }}
-                        className="meeting_text"
-                      ></div>
-                    </Col>
-                    <Col lg={8}>
-                      <div
-                        className="meeting_image"
-                        style={{
-                          position: "relative",
-                          height: "60vh",
-                          width: "100%",
-                          clipPath: "inset(0 0 0 0)",
-                        }}
-                      >
-                        <Image
-                          src={
-                            meetings?.meetingImage?.sourceUrl
-                          }
-                          fill
-                          style={{ height: "100%", width: "100%" }}
-                          alt={
-                            meetings?.meetingImage?.altText
-                          }
-                        />
-                      </div>
-                    </Col>
-                  </Row>
-                </Container>
-              )}
-            </div>
+                  </div>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: meetings?.meetingDescription,
+                    }}
+                    className="meeting_text"
+                  ></div>
+                </Col>
+                <Col lg={8}>
+                  <div
+                    className="meeting_image"
+                    style={{
+                      position: "relative",
+                      height: "60vh",
+                      width: "100%",
+                      clipPath: "inset(0 0 0 0)",
+                    }}
+                  >
+                    <Image
+                      src={meetings?.meetingImage?.sourceUrl}
+                      fill
+                      style={{ height: "100%", width: "100%" }}
+                      alt={meetings?.meetingImage?.altText}
+                    />
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          )}
+        </div>
       </section>
     </>
   );
--- src/components/SplitImageRight.tsx
+++ src/components/SplitImageRight.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button, Col, Container, Row } from "react-bootstrap";
 
-
 type MyProps = {
   splitImagesRight: any;
 };
@@ -12,60 +11,44 @@ const SplitImageRight = (props: MyProps) => {
 
   return (
     <>
-          <section className="split_section">
-              <Container>
-                <Row className="flex-row-reverse">
-                  <Col lg={8}>
-                    <div className="split_image">
-                      <Image
-                        src={
-                          splitImagesRight
-                            ?.splitImage?.sourceUrl
-                        }
-                        fill
-                        alt={
-                          splitImagesRight
-                            ?.splitImage?.altText
-                        }
-                      />
-                    </div>
-                  </Col>
-                  <Col lg={4}>
-                    <div className="split_text">
-                      <h2
-                        dangerouslySetInnerHTML={{
-                          __html:
-                            splitImagesRight
-                              ?.splitTitle,
-                        }}
-                      ></h2>
-                      <p
-                        dangerouslySetInnerHTML={{
-                          __html:
-                            splitImagesRight
-                              ?.splitDescription,
-                        }}
-                      ></p>
-                      {splitImagesRight
-                        ?.splitButton == null ? (
-                        ""
-                      ) : (
-                        <Link
-                          href={
-                            splitImagesRight
-                              ?.splitButton.url
-                          }
-                        >
-                          <Button className="SplitBtn">
-                            Get <span>Approved</span>
-                          </Button>
-                        </Link>
-                      )}
-                    </div>
-                  </Col>
-                </Row>
-              </Container>
-          </section>
+      <section className="split_section">
+        <Container>
+          <Row className="flex-row-reverse">
+            <Col lg={8}>
+              <div className="split_image">
+                <Image
+                  src={splitImagesRight?.splitImage?.sourceUrl}
+                  fill
+                  alt={splitImagesRight?.splitImage?.altText}
+                />
+              </div>
+            </Col>
+            <Col lg={4}>
+              <div className="split_text">
+                <h2
+                  dangerouslySetInnerHTML={{
+                    __html: splitImagesRight?.splitTitle,
+                  }}
+                ></h2>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: splitImagesRight?.splitDescription,
+                  }}
+                ></p>
+                {splitImagesRight?.splitButton == null ? (
+                  ""
+                ) : (
+                  <Link href={splitImagesRight?.splitButton.url}>
+                    <Button className="SplitBtn">
+                      Get <span>Approved</span>
+                    </Button>
+                  </Link>
+                )}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </>
   );
 };
