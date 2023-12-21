f2e74936f6815fa0fd59528328363617a2394cb9
Arif <iariful950@gmail.com>
2023-12-21 21:38:10 +0600

    changed homebuyer section to previous one

--- src/components/HomeBuyerNewBC.tsx
+++ src/components/HomeBuyerNewBC.tsx
@@ -7,24 +7,11 @@ type MyProps = {
 };
 export default function HomeBuyerNewBC(props: MyProps) {
   const { advisorData } = props;
-  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   const [windowWidth, setWindowWidth] = useState(
     typeof window !== "undefined" ? window.innerWidth : 0
   );
   const [colGap, setColGap] = useState({});
-
-  const sliceDescription = (description: string, maxLength: number): string => {
-    // return `${description.slice(0, maxLength)}...`;
-    const words = description.split(" ");
-    // console.log("words", words);
-    if (words.length <= maxLength) {
-      return `${description}...`;
-    } else {
-      const slicedWords = words.slice(0, maxLength).join(" ");
-      return `${slicedWords}...`;
-    }
-  };
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -74,42 +61,20 @@ export default function HomeBuyerNewBC(props: MyProps) {
         >
           {advisorData?.advisorCards?.map((data, index) => {
             return (
-              <div
-                onMouseOver={() => setHoveredIndex(index)}
-                onMouseOut={() => setHoveredIndex(null)}
-                key={index}
-                className="card text-center"
-              >
+              <div key={index} className="card text-center">
                 <div className="card-index">
                   <p className="">{index + 1}</p>
                 </div>
                 <h3 className="card-title">{data?.title}</h3>
-                <div className="card-body card-pc text-center">
-                  {index === hoveredIndex ? (
+                <div className="card-body text-center">
+                  <div className="card-body text-center">
                     <div
                       className="text-center"
                       dangerouslySetInnerHTML={{
                         __html: data?.description,
                       }}
                     ></div>
-                  ) : (
-                    <div>
-                      <div
-                        className="text-center"
-                        dangerouslySetInnerHTML={{
-                          __html: sliceDescription(data?.description, 15),
-                        }}
-                      ></div>
-                    </div>
-                  )}
-                </div>
-                <div className="card-body card-mobile text-center">
-                  <div
-                    className="text-center"
-                    dangerouslySetInnerHTML={{
-                      __html: data?.description,
-                    }}
-                  ></div>
+                  </div>
                 </div>
               </div>
             );
--- src/components/HomeBuyerSection.tsx
+++ src/components/HomeBuyerSection.tsx
@@ -6,17 +6,7 @@ type MyProps = {
 };
 export default function HomeBuyerSection(props: MyProps) {
   const { homebuyerData } = props;
-  const [hoveredIndex, setHoveredIndex] = useState(null);
-  const sliceDescription = (description: string, maxLength: number): string => {
-    // return `${description.slice(0, maxLength)}...`;
-    const words = description.split(" ");
-    if (words.length <= maxLength) {
-      return description;
-    } else {
-      const slicedWords = words.slice(0, maxLength).join(" ");
-      return `${slicedWords}...`;
-    }
-  };
+  
   return (
     <section className="my-5">
       <Container>
@@ -29,42 +19,20 @@ export default function HomeBuyerSection(props: MyProps) {
         <div className="row row-cols-1 row-cols-md-4 g-4 homebuyer-items mt-5">
           {homebuyerData?.advisorCards?.map((data, index) => {
             return (
-              <div
-                onMouseOver={() => setHoveredIndex(index)}
-                onMouseOut={() => setHoveredIndex(null)}
-                key={index}
-                className="card text-center"
-              >
+              <div key={index} className="card text-center">
                 <div className="card-index">
                   <p className="">{index + 1}</p>
                 </div>
                 <h3 className="card-title">{data?.title}</h3>
-                <div className="card-body card-pc text-center">
-                  {index === hoveredIndex ? (
+                <div className="card-body text-center">
+                  <div className="card-body text-center">
                     <div
                       className="text-center"
                       dangerouslySetInnerHTML={{
                         __html: data?.description,
                       }}
                     ></div>
-                  ) : (
-                    <div>
-                      <div
-                        className="text-center"
-                        dangerouslySetInnerHTML={{
-                          __html: sliceDescription(data?.description, 20),
-                        }}
-                      ></div>
-                    </div>
-                  )}
-                </div>
-                <div className="card-body card-mobile text-center">
-                  <div
-                    className="text-center"
-                    dangerouslySetInnerHTML={{
-                      __html: data?.description,
-                    }}
-                  ></div>
+                  </div>
                 </div>
               </div>
             );
