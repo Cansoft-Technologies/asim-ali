ae034266493b22cba58ff61e4d9c84c2aee29a38
Arif <iariful950@gmail.com>
2023-12-21 16:54:19 +0600

    updated ui issues

--- src/components/HomeBuyerNewBC.tsx
+++ src/components/HomeBuyerNewBC.tsx
@@ -7,10 +7,24 @@ type MyProps = {
 };
 export default function HomeBuyerNewBC(props: MyProps) {
   const { advisorData } = props;
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
   const [windowWidth, setWindowWidth] = useState(
     typeof window !== "undefined" ? window.innerWidth : 0
   );
   const [colGap, setColGap] = useState({});
+
+  const sliceDescription = (description: string, maxLength: number): string => {
+    // return `${description.slice(0, maxLength)}...`;
+    const words = description.split(" ");
+    // console.log("words", words);
+    if (words.length <= maxLength) {
+      return `${description}...`;
+    } else {
+      const slicedWords = words.slice(0, maxLength).join(" ");
+      return `${slicedWords}...`;
+    }
+  };
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -38,7 +52,9 @@ export default function HomeBuyerNewBC(props: MyProps) {
   return (
     <section className="my-5">
       <Container>
-        <div style={{ padding: "0 10px", maxWidth: "1100px", margin: "0 auto" }}>
+        <div
+          style={{ padding: "0 10px", maxWidth: "1100px", margin: "0 auto" }}
+        >
           <div
             className="text-center"
             dangerouslySetInnerHTML={{
@@ -58,18 +74,34 @@ export default function HomeBuyerNewBC(props: MyProps) {
         >
           {advisorData?.advisorCards?.map((data, index) => {
             return (
-              <div key={index} className="card text-center">
+              <div
+                onMouseOver={() => setHoveredIndex(index)}
+                onMouseOut={() => setHoveredIndex(null)}
+                key={index}
+                className="card text-center"
+              >
                 <div className="card-index">
                   <p className="">{index + 1}</p>
                 </div>
                 <h3 className="card-title">{data?.title}</h3>
                 <div className="card-body text-center">
-                  <div
-                    className="text-center"
-                    dangerouslySetInnerHTML={{
-                      __html: data?.description,
-                    }}
-                  ></div>
+                  {index === hoveredIndex ? (
+                    <div
+                      className="text-center"
+                      dangerouslySetInnerHTML={{
+                        __html: data?.description,
+                      }}
+                    ></div>
+                  ) : (
+                    <div>
+                      <div
+                        className="text-center"
+                        dangerouslySetInnerHTML={{
+                          __html: sliceDescription(data?.description, 20),
+                        }}
+                      ></div>
+                    </div>
+                  )}
                 </div>
               </div>
             );
