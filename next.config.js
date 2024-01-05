8a43f30ea9a9e1b55182dffde95723d6e289b826
Arif <iariful950@gmail.com>
2024-01-06 02:13:46 +0600

    removed blogs page and updated single blog page

--- next.config.js
+++ next.config.js
@@ -1,94 +1,97 @@
-const { withFaust } = require('@faustjs/next');
+const { withFaust } = require("@faustjs/next");
 const runtimeCaching = require("next-pwa/cache");
-const prod = process.env.NODE_ENV === 'production'
+const prod = process.env.NODE_ENV === "production";
 /**
  * @type {import('next').NextConfig}
  **/
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   disable: prod ? false : true,
   skipWaiting: true,
   runtimeCaching,
-})
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer ( withFaust ( withPWA({
-  optimizeImages: true,
-    images: {
-      domains: ["localhost","asimaliprod.wpengine.com"],
-      optimized: true,
-      allowFutureImage: true,
-    },
-    async redirects() {
-      return [
-        {
-          source: "/b-lender-mortgage-bc",
-          destination: "/b-lender-bc-mortgage",
-          permanent: true,
-        },
-        {
-          source: "/refinance-your-mortgage-in-vancouver",
-          destination: "https://asimali.ca",
-          permanent: true,
-        },
-        {
-          source: "/best-mortgage-rates-in-vancouver",
-          destination: "https://asimali.ca",
-          permanent: true,
-        },
-        {
-          source: "/vancouver-empty-home-tax",
-          destination: "https://asimali.ca",
-          permanent: true,
-        },
-        {
-          source: "/vancity-mortgage-calculator",
-          destination: "https://asimali.ca",
-          permanent: true,
-        },
-        {
-          source: "/mortgage-fund-canada",
-          destination: "https://asimali.ca",
-          permanent: true,
-        },
-        {
-          source: "/our-locations",
-          destination: "https://asimali.ca",
-          permanent: false,
-        },
-        {
-          source: "/borrow-down-bc",
-          destination: "https://asimali.ca/borrow-down-payment-services",
-          permanent: false,
-        },
-      ];
-    },
-    async headers() {
-      return [
-        {
-          source: "/:all*(svg|jpg|png|webp|jpeg)",
-          locale: false,
-          headers: [
-            {
-              key: "Cache-Control",
-              value: 'public, max-age=9999999999, must-revalidate',
-            },
-          ],
-        },
-      ];
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    }
-})));
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-
-
-
-
-
+module.exports = withBundleAnalyzer(
+  withFaust(
+    withPWA({
+      optimizeImages: true,
+      images: {
+        domains: ["localhost", "asimaliprod.wpengine.com"],
+        optimized: true,
+        allowFutureImage: true,
+      },
+      async redirects() {
+        return [
+          {
+            source: "/b-lender-mortgage-bc",
+            destination: "/b-lender-bc-mortgage",
+            permanent: true,
+          },
+          {
+            source: "/refinance-your-mortgage-in-vancouver",
+            destination: "https://asimali.ca",
+            permanent: true,
+          },
+          {
+            source: "/best-mortgage-rates-in-vancouver",
+            destination: "https://asimali.ca",
+            permanent: true,
+          },
+          {
+            source: "/vancouver-empty-home-tax",
+            destination: "https://asimali.ca",
+            permanent: true,
+          },
+          {
+            source: "/vancity-mortgage-calculator",
+            destination: "https://asimali.ca",
+            permanent: true,
+          },
+          {
+            source: "/mortgage-fund-canada",
+            destination: "https://asimali.ca",
+            permanent: true,
+          },
+          {
+            source: "/our-locations",
+            destination: "https://asimali.ca",
+            permanent: false,
+          },
+          {
+            source: "/borrow-down-bc",
+            destination: "https://asimali.ca/borrow-down-payment-services",
+            permanent: false,
+          },
+          {
+            source: "/blogs",
+            destination: "https://asimali.ca/blog",
+            permanent: false,
+          },
+        ];
+      },
+      async headers() {
+        return [
+          {
+            source: "/:all*(svg|jpg|png|webp|jpeg)",
+            locale: false,
+            headers: [
+              {
+                key: "Cache-Control",
+                value: "public, max-age=9999999999, must-revalidate",
+              },
+            ],
+          },
+        ];
+      },
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+    })
+  )
+);
