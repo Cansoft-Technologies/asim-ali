62c114d0d7ae8b646264f1d3dd484ffa9684b318
Pulok Chowdhury <riopulok@gmail.com>
2024-07-02 01:02:29 +0600

    added mega and ui fix

--- src/components/MortgageAdvisor.tsx
+++ src/components/MortgageAdvisor.tsx
@@ -8,7 +8,8 @@ type MyProps = {
 export default function MortgageAdvisor(props: MyProps) {
   const { advisorData } = props;
   return (
-    <section className="split_section mt-5">
+    <>
+    <section className="split_section mt-5 d-none d-md-block">
       <Container>
         <Row>
           <Col lg={6}>
@@ -67,5 +68,65 @@ export default function MortgageAdvisor(props: MyProps) {
         </Row>
       </Container>
     </section>
+    <section className="split_section mt-5 d-block d-md-none ">
+      <Container>
+        <Row>
+        <Col lg={6}>
+            {advisorData?.advisorCards?.map((data, item) => {
+              return (
+                <div key={item} className="split_text mb-0 text-center">
+                  <h3 className="advisor-title">{data.title}</h3>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: data.description,
+                    }}
+                    className="renovation-content-list"
+                  ></div>
+                </div>
+              );
+            })}
+            <div
+              dangerouslySetInnerHTML={{
+                __html: advisorData?.advisorDescriptionBottom,
+              }}
+              className="service-description"
+            ></div>
+          </Col>
+          <Col lg={6}>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: advisorData?.advisorTitle,
+              }}
+              className="text-start service-title"
+            ></div>
+            <h3 className="advisor-title">
+              {advisorData?.advisorDescriptionTitle}
+            </h3>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: advisorData?.advisorDescriptionTop,
+              }}
+              className=""
+            ></div>
+            <div className="split_image">
+              <Image
+                src={advisorData?.advisorImage?.sourceUrl}
+                width="390"
+                      height="400"
+                alt={advisorData?.advisorImage?.altText}
+                priority={true}
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                      }}
+                      quality={100}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    </>
   );
 }
