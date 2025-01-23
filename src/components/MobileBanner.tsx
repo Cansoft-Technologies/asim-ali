1432302445859611c881cff5fba666b78241f30c
Pulok Chowdhury <riopulok@gmail.com>
2025-01-23 21:08:09 +0600

    added location fix

--- src/components/MobileBanner.tsx
+++ src/components/MobileBanner.tsx
@@ -34,6 +34,7 @@ const MobileBanner = (props: MyProps) => {
               style={{
                 objectFit: "cover",
               }}
+              priority={true}
             />
             <div
               style={{
@@ -80,7 +81,7 @@ const BannerInfo = ({ slide }) => {
                 }`}
               >
                 <Button className={styles.bannerBtn}>
-                  Contact <span>Us</span>
+                  <span>Contact Us</span>
                 </Button>
               </Link>
               <Link href="/apply-now">
