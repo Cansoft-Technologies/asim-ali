8f4bddc9dbbd29a7e5fb33c41b940820ad4ab30b
AB MisHkat <mishkat606@gmail.com>
2025-07-03 20:08:47 +0600

    sticky bar and image update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,7 @@ import localFont from 'next/font/local';
 import { useRouter } from "next/router";
 import Script from "next/script";
 import { useReportWebVitals } from 'next/web-vitals';
+import StickyCtaTab from '../components/StickyCTA';
 
 // Load RB Magnat Neue Test as local font
 const rbMagnat = localFont({
@@ -77,24 +78,25 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   get_country_code("https://get.geojs.io/v1/ip/country.json");
   return (
     <main className={`${rbMagnat.variable} ${openSans.variable} ${helvetica.variable} ${oswald.variable} font-sans`}>
+      <StickyCtaTab />
       <FaustProvider client={client} pageProps={pageProps}>
-    {/* Clarity Tracking Script */}
-    <Script
-        id="clarity-script"
-        strategy="lazyOnload"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "o530f5nvx2");
-          `,
-        }}
-      />
+        {/* Clarity Tracking Script */}
+        <Script
+          id="clarity-script"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "o530f5nvx2");
+            `,
+          }}
+        />
         <Component {...pageProps} />
       </FaustProvider>
-      </main>
+    </main>
   );
 }
 
